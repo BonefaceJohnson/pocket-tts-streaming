69b712109e2f1d0602d596e393fc1fe72243e5f5
BonefaceJohnson <48912825+BonefaceJohnson@users.noreply.github.com>
2026-04-27 21:32:15 +0200

    Refactor wyoming_server.py by reordering imports

--- pocket-tts-streaming/wyoming_server.py
+++ pocket-tts-streaming/wyoming_server.py
@@ -379,7 +379,7 @@ async def main():
     
     try:
         _LOGGER.info("Loading Pocket TTS model weights...")
-        model = TTSModel.load_model(language=german)
+        model = TTSModel.load_model(language='german')
         
         # Process pending .wav files on startup
         for wav_path in CFG["voices_dir"].glob("*.wav"):
